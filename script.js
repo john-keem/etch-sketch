6661360fecb226ac86670b2c6d63d57d6df4637d
John <rladygks02@gmail.com>
2025-11-14 17:34:41 -0800

    Create hovering effect on the sketch board. Each square will change to black when hovered over and stay black until page is refreshed.

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ const container = document.querySelector("#container");
 for(let i = 0; i < GRID_DIMENSION * GRID_DIMENSION; i++) {
   let gridSlot = document.createElement("div");
   gridSlot.setAttribute("style", "height: 50px; width: 50px; border: solid; border-color: lightgray; border-width: 1px;");
+  gridSlot.addEventListener("mouseenter", () => gridSlot.style.backgroundColor = "black");
   container.appendChild(gridSlot);
 }
-
