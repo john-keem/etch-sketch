a5dc7eb4c82dc47dba9fe22993dd6ac628e3cb64
John <rladygks02@gmail.com>
2025-11-14 18:44:59 -0800

    Make request button functional. Use Javascript to call makeGrid function if valid user input is provided.

--- script.js
+++ script.js
@@ -2,10 +2,35 @@ const GRID_DIMENSION = 16;
 
 const container = document.querySelector("#container");
 
-for(let i = 0; i < GRID_DIMENSION * GRID_DIMENSION; i++) {
-  let gridSlot = document.createElement("div");
-  gridSlot.setAttribute("style", "height: 50px; width: 50px; border: solid; border-color: lightgray; border-width: 1px;");
-  gridSlot.addEventListener("mouseenter", () => gridSlot.style.backgroundColor = "black");
-  container.appendChild(gridSlot);
+function makeGrid(dimension) {
+  container.textContent = "";
+  const squareSize = 850 / dimension;
+
+  for(let i = 0; i < dimension * dimension; i++) {
+    let gridSlot = document.createElement("div");
+    gridSlot.classList.add("squares");
+    gridSlot.setAttribute("style", "border: solid; border-color: lightgray; border-width: 1px;");
+    gridSlot.style.width =  `${squareSize}px`;
+    gridSlot.style.height = `${squareSize}px`;
+    gridSlot.addEventListener("mouseenter", () => gridSlot.style.backgroundColor = "black");
+    container.appendChild(gridSlot);
+  }
 }
 
+const button = document.querySelector("button");
+button.addEventListener("click", request);
+
+function request() {
+  let userInput = Number(prompt("Please enter the number of squares per side for the new grid"));
+
+  if(isNaN(userInput) || userInput > 100 || userInput < 1) {
+    alert("Must be a number less than 100 and greater than 1");
+    return;
+  }
+
+  else {
+    makeGrid(userInput);
+  }
+}
+
+makeGrid(GRID_DIMENSION);
